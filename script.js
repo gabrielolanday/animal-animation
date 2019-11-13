f0f1139bfb47b89e2464f00976229a1f6172f4e3
MARS <gjolanday@gmail.com>
2019-11-13 01:24:38 -0500

    first

---
+++ script.js
@@ -0,0 +1,28 @@
+var hat = document.getElementById("hat");
+var moving = false;
+
+hat.addEventListener("mousedown", initialClick, false);
+
+
+function move(e){
+
+  var newX = e.clientX - 35;
+  var newY = e.clientY - 35;
+
+  hat.style.left = newX + "px";
+  hat.style.top = newY + "px";
+}
+
+function initialClick(e) {
+
+  if(moving){
+    document.removeEventListener("mousemove", move);
+    moving = !moving;
+    return;
+  }
+
+  moving = !moving;
+
+  document.addEventListener("mousemove", move, false);
+
+}
